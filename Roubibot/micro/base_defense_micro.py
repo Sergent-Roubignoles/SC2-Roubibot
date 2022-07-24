1d58c8a8a4ebd3bb613d02c42dc17fc53aba4872
Pierre Mercier <pime2121@gmail.com>
2022-07-24 09:22:59 -0400

    Added overlord air/ground threats

--- Roubibot/micro/base_defense_micro.py
+++ Roubibot/micro/base_defense_micro.py
@@ -1,11 +1,22 @@
+from sc2.units import Units
+
 from sc2.bot_ai import BotAI
 from sc2.ids.unit_typeid import UnitTypeId
 
+ground_threats: Units
+air_threats: Units
 
-def drone_self_defense(bot: BotAI):
+def defense_routine(bot: BotAI):
+    global ground_threats
+    global air_threats
+
     ground_threats = bot.enemy_units.filter(lambda enemy: not enemy.is_flying)
     air_threats = bot.enemy_units.filter(lambda enemy: enemy.is_flying)
 
+    drone_self_defense(bot)
+
+
+def drone_self_defense(bot: BotAI):
     if ground_threats.amount > 0:
         for drone in bot.workers:
             closest_threat = ground_threats.closest_to(drone)
